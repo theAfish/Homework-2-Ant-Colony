bc6554345a2aa499f28fe10ea841c725c5c596f5
theAfish <85424334+theAfish@users.noreply.github.com>
2021-10-09 14:36:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-import taichi as ti
+from AntColony import *
 
 ti.init(arch=ti.gpu)
 
-n = 320
-pixels = ti.field(dtype=float, shape=(n * 2, n))
 
-@ti.func
-def complex_sqr(z):
-    return ti.Vector([z[0]**2 - z[1]**2, z[1] * z[0] * 2])
+rdr = Renderer(512, 512)
 
-@ti.kernel
-def paint(t: float):
-    for i, j in pixels:  # Parallelized over all pixels
-        c = ti.Vector([-0.8, ti.cos(t) * 0.2])
-        z = ti.Vector([i / n - 1, j / n - 0.5]) * 2
-        iterations = 0
-        while z.norm() < 20 and iterations < 50:
-            z = complex_sqr(z) + c
-            iterations += 1
-        pixels[i, j] = 1 - iterations * 0.02
+ph = Detectables(rdr, 0.0017, 2.0, 2.0)
+pf = Detectables(rdr, 0.0017, 2.0, 2.0)
+ants = Ants(1500, 0.0006, pf, ph, 0.25)
+ac = AntColony(rdr, ants, ph, pf)
 
-gui = ti.GUI("Julia Set", res=(n * 2, n))
-
-for i in range(1000000):
-    paint(i * 0.03)
-    gui.set_image(pixels)
-    gui.show()
+if __name__ == "__main__":
+    ac.run()
