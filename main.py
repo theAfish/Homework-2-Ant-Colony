fc009a78a3ec43b964b69f8c8385341484eee5ef
theAfish <85424334+theAfish@users.noreply.github.com>
2022-01-18 10:32:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from AntColony import *
 
 ti.init(arch=ti.gpu)
-
+dt = 1e-3
 
 rdr = Renderer(512, 512)
 
-ph = Detectables(rdr, 0.0017, 2.0, 2.0)
-pf = Detectables(rdr, 0.0017, 2.0, 2.0)
-ants = Ants(1500, 0.0006, pf, ph, 0.25)
+ph = Detectables(rdr, 0.2*dt, 1.0, 1.0)
+pf = Detectables(rdr, 0.2*dt, 2.0, 2.0)
+ants = Ants(2000, 1.0, pf, ph, 1.5, 10.0)
 ac = AntColony(rdr, ants, ph, pf)
 
 if __name__ == "__main__":
