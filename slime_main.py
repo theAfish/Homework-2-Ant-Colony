bf3a4c26f007217446476d79301c8859d2624f33
FantasyVR <y2505418927@gmail.com>
2022-02-24 09:59:52 +0800

    format

--- slime_main.py
+++ slime_main.py
@@ -5,8 +5,8 @@ dt = 1e-3
 
 rdr = Renderer(600, 600, "Slime Simulation")
 
-ph = Detectables(rdr, 5.0*dt, 1.0, 1.0)
-pf = Detectables(rdr, 5.0*dt, 1.0, 1.0)
+ph = Detectables(rdr, 5.0 * dt, 1.0, 1.0)
+pf = Detectables(rdr, 5.0 * dt, 1.0, 1.0)
 ants = Ants(50000, 1.0, pf, ph, 0.1, 10.0)
 ac = AntColony(rdr, ants, ph, pf)
 
